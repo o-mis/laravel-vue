1dab82e634989a1a9e2744ff97d22aa5212c35ee
Mayo <memberwws@gmail.com>
2021-01-15 21:03:59 +0900

    [add] Vue導入

---
+++ backend/resources/js/components/TestComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">成功した！</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("初のVue.js!");
+    },
+};
+</script>
